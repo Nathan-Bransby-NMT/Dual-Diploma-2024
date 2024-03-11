cd13073fa910c0e8fcb489f5c64223d7c9fd972b
Nathan Bransby <141193650+Nathan-Bransby-NMT@users.noreply.github.com>
2024-03-12 01:59:58 +0800

    update dir

--- Semester-1/Customise-CMS-Wordpress/Practice-Files/Child-theme/08-mychildtheme/index.php
+++ Semester-1/Customise-CMS-Wordpress/Practice-Files/Child-theme/08-mychildtheme/index.php
@@ -8,14 +8,15 @@
 get_header(); ?>
 
 <div class="wrap">
+
 	<?php if ( is_home() && ! is_front_page() ) : ?>
 		<header class="page-header">
 			<h1 class="page-title"><?php single_post_title(); ?></h1>
 		</header>
 	<?php else : ?>
-	<header class="page-header">
-		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
-	</header>
+		<header class="page-header">
+			<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
+		</header>
 	<?php endif; ?>
 
 	<div id="primary" class="content-area">
