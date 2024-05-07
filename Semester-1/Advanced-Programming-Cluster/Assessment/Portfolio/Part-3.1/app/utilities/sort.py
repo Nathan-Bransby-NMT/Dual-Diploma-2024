2b476da8db73cf61fc324896daa8a254f2f1d778
Nathan Bransby <141193650+Nathan-Bransby-NMT@users.noreply.github.com>
2024-05-07 14:12:30 +0800

    Update: Keep working on TimSort algorithm.

--- Semester-1/Advanced-Programming-Cluster/Assessment/Portfolio/Part-3.1/app/utilities/sort.py
+++ Semester-1/Advanced-Programming-Cluster/Assessment/Portfolio/Part-3.1/app/utilities/sort.py
@@ -1,13 +1,20 @@
-def tim_sort(data: list) -> list:
+from typing import Optional
+
+
+def tim_sort(data: list, runs: Optional[int] = None) -> list:
+    """Converts
+    """
     # todo - implement from https://www.geeksforgeeks.org/timsort/
     if len(data) <= 1:
         # Nothing to sort against
         return data
+    elif 1 < len(data) < 17:
+        ...
     # min_run = ... todo -- <= the power on 2...
-    quater_length,
+    #quater_length
     try:
         # devide array into 
         ...
     except ValueError as e:
-        raise ValueError(f"Unable to compare data content: {e}" from e)
+        raise ValueError(f"Unable to compare data content.")
     
