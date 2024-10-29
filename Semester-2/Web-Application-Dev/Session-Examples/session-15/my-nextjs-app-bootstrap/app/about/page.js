e3ddb562dccb81ecfc064f0c443fe9af3000dca4
Nathan Bransby <141193650+Nathan-Bransby-NMT@users.noreply.github.com>
2024-10-29 11:59:11 +0800

    WebAppDev: Session-15 example files

---
+++ Semester-2/Web-Application-Dev/Session-Examples/session-15/my-nextjs-app-bootstrap/app/about/page.js
@@ -0,0 +1,9 @@
+"use client"; // remove this to generate the error
+
+export default function AboutPage() {
+  return (
+    <main className="container">
+      <h1>About Us</h1>
+    </main>
+  );
+}
